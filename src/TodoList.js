592d40dc407d4f9ac36b5f92d0b91887f668c6ba
Mariesungheekim <mariesunghee@gmail.com>
2018-12-07 23:14:51 -0500

    handler for submit item

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,7 +6,7 @@ export default class TodoList extends Component{
         return (
             <div className="todoListMain">
                 <div className="header">
-                    <form>
+                    <form onSubmit={this.props.addItem}>
                         <input placeholder="Task"/>
                         <button type="submit"> Add Task</button>
                     </form>
