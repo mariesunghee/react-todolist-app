f45604ce37e242aec159d8620a7ebb1d883b79a0
Mariesungheekim <mariesunghee@gmail.com>
2018-12-10 23:02:30 -0500

    add Item with ref

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <TodoList addItem={this.addItem}/>
-      </div>
-    );
-  }
+    constructor() {
+        super()
+        this.state = {
+            items: [],
+            currentItem:{text:'',key:''}
+        }
+        this.inputElement=React.createRef()
+    }
+
+    addItem = e => {
+        e.preventDefault()
+        console.log("ADDED ITEM")
+    }
+    handleInput= e =>{
+        console.log("Change Input")
+        const itemText = e.target.value
+        this.setState({
+            currentItem:{
+                text:itemText,
+                key:Date.now()
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <TodoList inputElemnet={this.inputElement} addItem={this.addItem} handleInput={this.handleInput}
+                currentItem={this.state.currentItem}/>
+
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 
 export default class TodoList extends Component{
     render(){
+
         return (
             <div className="todoListMain">
                 <div className="header">
                     <form onSubmit={this.props.addItem}>
-                        <input placeholder="Task"/>
+                        <input ref={this.props.inputElement} onChange={this.props.handleInput} placeholder="Task"/>
                         <button type="submit"> Add Task</button>
                     </form>
                 </div>
